7f547eb6527e8cbaf6522bb8e36e937c56712e75
Natchel <LebeaN0101@gmail.com>
2024-01-30 06:57:08 +0200

    Save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { TodoWrapper } from './components/TodoWrapper';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <TodoWrapper/>
-   
-    </div>
-  );
+    const [appState, setAppState] = useState({
+        todos: []
+    });
+
+    // Load the app state from localStorage on component mount
+    useEffect(() => {
+        const storedState = localStorage.getItem('todoAppState');
+        if (storedState) {
+            setAppState(JSON.parse(storedState));
+        }
+    }, []);
+
+    // Save the app state to localStorage whenever it changes
+    useEffect(() => {
+        localStorage.setItem('todoAppState', JSON.stringify(appState));
+    }, [appState]);
+
+    return (
+        <div className="App">
+            <TodoWrapper
+                appState={appState}
+                setAppState={setAppState}
+            />
+        </div>
+    );
 }
 
 export default App;
